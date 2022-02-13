5a4e021a2f5e49631b491325f1ce86d4f2897237
Cade Ellsworth <caedmd2100@gmail.com>
2022-02-13 14:31:42 -0700

    created basic server, npm installed all dependencies, created connection to mongodb, created User model, created gitignore

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS mco_ticket_exchange_db;
+
+CREATE DATABASE mco_ticket_exchange_db;
---
+++ server.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express(); 
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+const session = require('express-session');
+const sequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new sequelizeStore({
+        db: sequelize
+    })
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+
+app.use(require('./controllers/'));
+
+// turn on connection to db and start server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
+});
